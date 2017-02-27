f8819c15f3f88c6256a178ddeaeb976f6115a458
dasoares8 <david_j_soares@yahoo.com>
2017-02-26 21:30:46 -0500

    Added "last_visited_date" to visitee model, new query

--- app/models/visitee.py
+++ app/models/visitee.py
@@ -1,22 +1,55 @@
+from app import db
+from flask_table import Table, Col
+from sqlalchemy import func
 
+class VisiteeTable(Table):
+    last_name = Col('Last Name', td_html_attrs=())
+    first_name = Col('First Name')
+    email = Col('Email')
+    phone_number = Col('Phone Number')
+    location = Col('Location')
+    address = Col('Address')
+    last_visit_date = Col('LastVisitDate')
 
-class Visitee:
+class VisiteeModel(db.Model):
 
-    def __init__(self, visiteeLastName, visiteeFirstName, phoneNumber, location, address):
+    __tablename__ = 'visitees'
+    id = db.Column(db.Integer, primary_key = True)
+    last_name = db.Column(db.String(64))
+    first_name = db.Column(db.String(64))
+    email = db.Column(db.String(64))
+    phone_number = db.Column(db.String(16))
+    location = db.Column(db.String(64))
+    address = db.Column(db.String(128))
+    last_visit_date = db.Column(db.Date)
 
-        self.visiteeLastName = visiteeLastName
-        self.visiteeFirstName = visiteeFirstName
-        self.phoneNumber = phoneNumber
+    def __init__(self, last_name, first_name, email, phone_number, location, address, last_visit_date):
+
+        self.last_name = last_name
+        self.first_name = first_name
+        self.email = email
+        self.phone_number = phone_number
         self.location = location    # the place, for example "Christopher House"
         self.address = address
+        self.last_visit_date = last_visit_date
 
     def json(self):
-        # Don't send the password in the result, just the username
-        return {'visitee last name': self.visiteeLastName,
-                'visitee first name': self.visiteeFirstName,
-                'phone number': self.phoneNumber,
-                'location': self.location
+        return {'visitee last name': self.last_name,
+                'visitee first name': self.first_name,
+                'email': self.email,
+                'phone number': self.phone_number,
+                'location': self.location,
+                'address': self.address,
+                'last visit date': self.last_visit_date
                 }
+
+    @classmethod
+    def find_all_visitees(cls):
+        return cls.query.all()
+
+    @classmethod
+    def find_longest_waiting_visitee(cls):
+        return cls.query(func.min(VisiteeModel.last_visit_date))
 
     def add_visitee(self):
         # write json to file
@@ -28,5 +61,8 @@ class Visitee:
 
     def list_visitees_by_location(self):
         # if many visitees are in the same place (hospital, nursing home, etc.), list them
+        pass
+
+    def find_visitee_by_last_name(self):
         pass
 
